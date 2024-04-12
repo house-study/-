7c93d27a3a8e8fd0acf8fae8dafdf6276e43c60a
정내혁 <cnh0301@gmail.com>
2024-04-12 16:57:43 +0900

    정답 제출

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  */
 function reverseArray(arr) {
   // 여기에 코드를 작성하세요.
+  return arr
+    .map((item) => (Array.isArray(item) ? reverseArray(item) : item))
+    .reverse();
 }
 
 module.exports = reverseArray;
