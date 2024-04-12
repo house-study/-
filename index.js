ab6851d03d9513c7800fd8ec9e76bacca9cac26f
정내혁 <cnh0301@gmail.com>
2024-04-12 16:24:54 +0900

    chore: 정답 삭제

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
  */
 function reverseArray(arr) {
   // 여기에 코드를 작성하세요.
-  return arr
-    .map((item) => (Array.isArray(item) ? reverseArray(item) : item))
-    .reverse();
 }
 
 module.exports = reverseArray;
